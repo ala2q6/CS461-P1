84a8e6e44a9f4f018eae2473e0447dc86dab028c
highlander <alexarbuckle@protonmail.com>
2021-09-04 04:53:34 -0500

    further progress

--- CS461P1.py
+++ CS461P1.py
@@ -11,27 +11,36 @@ from random import shuffle
 if (__name__ == '__main__'):
 
     # while (not q, Q) <
-    while (input('[Enter]\tto continue\n[Q]\t\tto quit\n\nInput : ') not in ['q', 'Q']):
+    while (input('[C]ontinue\n[Q]uit\n\nInput : ') not in ['q', 'Q']):
 
         # Declaration <
-        value = {'1' : 4, '11' : 1, '12' : 3, '13' : 3}
-        suit, score = ('C', 'H', 'S', 'D'), [0, 0, 0, 0, 0]
+        score, suit = [0, 0, 0, 0, 0], ('C', 'H', 'S', 'D')
         deck = [f'{j}{i}' for j in range(1, 14) for i in suit]
+        cardCount, distCount = {'1' : 4, '11' : 1, '12' : 3, '13' : 3}, {2 : 1, 1 : 2, 0 : 5}
 
         # >
 
+        # Evaluate <
         shuffle(deck)
-        handA = [deck[i] for i in range(13)]
-        handB = [deck[i] for i in range(13, )]
+        a = [deck[i] for i in range(13)]
         [deck.pop(i) for i in range(13)]
+        for i in range(500):
+
+            shuffle(deck)
+            b, count = [deck[i] for i in range(13, 26)], 0
+
+            # Distribution Count <
+
+
+            # >
+
+            # High-Card Count <
+
+
+            # >
+
+        # >
 
     # >
-
-# >
-
-# Example <
-# ks 8s 6s 5s kh 10h 7h 5h 3h 7d jc 8c 6c
-# 3         + 3                + 1
-#                             + 2
 
 # >
