dbfe3dd9258907a5a7dcb36d43f81ee1895214a6
Alex Arbuckle <54282562+ala2q6@users.noreply.github.com>
2021-09-13 20:29:18 +0000

    distribution count added, high card count added, output added, count issue

--- CS461P1.py
+++ CS461P1.py
@@ -14,9 +14,10 @@ if (__name__ == '__main__'):
     while (input('[C]ontinue\n[Q]uit\n\nInput : ') not in ['q', 'Q']):
 
         # Declaration <
-        score, suit = [0, 0, 0, 0, 0], ('C', 'H', 'S', 'D')
+        suit, sim = ('C', 'H', 'S', 'D'), 500
         deck = [f'{j}{i}' for j in range(1, 14) for i in suit]
-        cardCount, distCount = {'1' : 4, '11' : 1, '12' : 3, '13' : 3}, {2 : 1, 1 : 2, 0 : 5}
+        total = {k : 0 for k in ['Pass', 'Part', 'Game', 'Small Slam', 'Grand Slam']}
+        cardDict, distDict = {'1' : 4, '11' : 1, '12' : 3, '13' : 3}, {2 : 1, 1 : 2, 0 : 5}
 
         # >
 
@@ -24,26 +25,44 @@ if (__name__ == '__main__'):
         shuffle(deck)
         a = [deck[i] for i in range(13)]
         [deck.pop(i) for i in range(13)]
-        for i in range(500):
+        for i in range(sim):
 
             shuffle(deck)
-            b, count = [deck[i] for i in range(13, 26)], 0
-            for d in [a, b]:
+            b, score = [deck[i] for i in range(13, 26)], 0
+            for h in [a, b]:
 
                 # Distribution Count <
-                c = [i[-1] for i in d]
+                ab = [i[-1] for i in h]
+                score += sum([distDict[ab.count(s)] for s in suit if (ab.count(s) in distDict.keys())])
 
                 # >
 
                 # High-Card Count <
-                c = [i[0] + i[1] for i in d]
+                ab = [i[0] + i[1] for i in h]
+                score += sum([cardDict[c] for c in ab if (c in cardDict.keys())])
 
                 # >
 
-            #count += sum([distCount[(a + b).count(s)] for s in suit if ((a + b).count(s) in distCount.keys())])
+                print(score)
+                input('; ')
+
+            # Update Total <
+            if (31 < score < 36): total['Small Slam'] += score
+            if (score > 35): total['Grand Slam'] += score
+            if (19 < score < 26): total['Part'] += score
+            if (25 < score < 32): total['Game'] += score
+            if (score < 20): total['Pass'] += score
+
+            # >
+ 
+        # >
+
+        # Output <
+        strVariable = '\n'.join([f'{k} : {v / sim}%' for k, v in total.items()])
+        print(f'\nResult :\n\n{strVariable}\n')
 
         # >
 
-    # >
+    # >    
 
 # >
