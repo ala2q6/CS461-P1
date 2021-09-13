9a5301583c33b8aaad5da3773c2e9859c6c211e3
Alex Arbuckle <54282562+ala2q6@users.noreply.github.com>
2021-09-13 20:50:33 +0000

    high card count test issue

--- CS461P1.py
+++ CS461P1.py
@@ -31,6 +31,8 @@ if (__name__ == '__main__'):
             b, score = [deck[i] for i in range(13, 26)], 0
             for h in [a, b]:
 
+                print(h)
+
                 # Distribution Count <
                 ab = [i[-1] for i in h]
                 score += sum([distDict[ab.count(s)] for s in suit if (ab.count(s) in distDict.keys())])
@@ -39,26 +41,37 @@ if (__name__ == '__main__'):
 
                 # High-Card Count <
                 ab = [i[0] + i[1] for i in h]
-                score += sum([cardDict[c] for c in ab if (c in cardDict.keys())])
+                #score += sum([cardDict[c] for c in ab if (c in cardDict.keys())])
+                print('original', sum([cardDict[c] for c in ab if (c in cardDict.keys())]))
+
+                for c in ab:
+
+                  if (c in cardDict.keys()):
+
+                    score += cardDict[c]
+                  
+                  else:
+
+                    print('ok')
+                
+                print('try', score)
+                input('; ')
 
                 # >
 
-                print(score)
-                input('; ')
-
             # Update Total <
-            if (31 < score < 36): total['Small Slam'] += score
-            if (score > 35): total['Grand Slam'] += score
-            if (19 < score < 26): total['Part'] += score
-            if (25 < score < 32): total['Game'] += score
-            if (score < 20): total['Pass'] += score
+            if (31 < score < 36): total['Small Slam'] += 1
+            if (score > 35): total['Grand Slam'] += 1
+            if (19 < score < 26): total['Part'] += 1
+            if (25 < score < 32): total['Game'] += 1
+            if (score < 20): total['Pass'] += 1
 
             # >
  
         # >
 
         # Output <
-        strVariable = '\n'.join([f'{k} : {v / sim}%' for k, v in total.items()])
+        strVariable = '\n'.join([f'{k} : {v}%' for k, v in total.items()])
         print(f'\nResult :\n\n{strVariable}\n')
 
         # >
