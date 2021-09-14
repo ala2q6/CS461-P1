42b3516f1a7da787f1e1cd835900491d8cc53e37
highlander <alexarbuckle@protonmail.com>
2021-09-14 16:52:44 -0500

    concatenation subbed for slicing

--- CS461P1.py
+++ CS461P1.py
@@ -10,14 +10,17 @@ from random import shuffle
 # Main <
 if (__name__ == '__main__'):
 
-    # while (not q, Q) <
-    while (input('[C]ontinue\n[Q]uit\n\nInput : ') not in ['q', 'Q']):
+    # while ![q, Q] <
+    print('Monte Carlo Simulation\n')
+    while (input('[C]ontinue\n[Q]uit\n\nInput: ') not in ['q', 'Q']):
 
         # Declaration <
-        suit, sim = ('C', 'H', 'S', 'D'), 500
+        suit, sim = ('C', 'H', 'S', 'D'), 1000
         deck = [f'{j}{i}' for j in range(1, 14) for i in suit]
-        total = {k : 0 for k in ['Pass', 'Part', 'Game', 'Small Slam', 'Grand Slam']}
-        cardDict, distDict = {'1' : 4, '11' : 1, '12' : 3, '13' : 3}, {2 : 1, 1 : 2, 0 : 5}
+        total = {k : 0 for k in ['Pass', 'Part', 'Game', 'Small', 'Grand']}
+        highDict, distDict = {'1' : 4, '11' : 1, '12' : 2, '13' : 3}, {2 : 1, 1 : 2, 0 : 5}
+        highFunc = lambda hand : sum([highDict[card] for card in hand if (card in highDict.keys())])
+        distFunc = lambda h : sum([distDict[h.count(s)] for s in suit if (h.count(s) in distDict.keys())])
 
         # >
 
@@ -25,57 +28,30 @@ if (__name__ == '__main__'):
         shuffle(deck)
         a = [deck[i] for i in range(13)]
         [deck.pop(i) for i in range(13)]
+        ac = distFunc([c[-1] for c in a]) + highFunc([c[:-1] for c in a])
         for i in range(sim):
 
             shuffle(deck)
-            b, score = [deck[i] for i in range(13, 26)], 0
-            for h in [a, b]:
+            b, score = [deck[c] for c in range(13, 26)], 0
+            bc = distFunc([c[-1] for c in b]) + highFunc([c[:-1] for c in b])
 
-                print(h)
-
-                # Distribution Count <
-                ab = [i[-1] for i in h]
-                score += sum([distDict[ab.count(s)] for s in suit if (ab.count(s) in distDict.keys())])
-
-                # >
-
-                # High-Card Count <
-                ab = [i[0] + i[1] for i in h]
-                #score += sum([cardDict[c] for c in ab if (c in cardDict.keys())])
-                print('original', sum([cardDict[c] for c in ab if (c in cardDict.keys())]))
-
-                for c in ab:
-
-                  if (c in cardDict.keys()):
-
-                    score += cardDict[c]
-                  
-                  else:
-
-                    print('ok')
-                
-                print('try', score)
-                input('; ')
-
-                # >
-
-            # Update Total <
-            if (31 < score < 36): total['Small Slam'] += 1
-            if (score > 35): total['Grand Slam'] += 1
-            if (19 < score < 26): total['Part'] += 1
-            if (25 < score < 32): total['Game'] += 1
-            if (score < 20): total['Pass'] += 1
+            # Update <
+            if (31 < (ac + bc) < 36): total['Small'] += 1
+            if (25 < (ac + bc) < 32): total['Game'] += 1
+            if (19 < (ac + bc) < 26): total['Part'] += 1
+            if ((ac + bc) > 35): total['Grand'] += 1
+            if ((ac + bc) < 20): total['Pass'] += 1
 
             # >
- 
+
         # >
 
         # Output <
-        strVariable = '\n'.join([f'{k} : {v}%' for k, v in total.items()])
-        print(f'\nResult :\n\n{strVariable}\n')
+        strVariable = '\n'.join([f'{k}\t{v}' for k, v in total.items()])
+        print(f'\nHand: {a}\nCount: {ac}\nResult:\n\n{strVariable}\n\n')
 
         # >
 
-    # >    
+    # >
 
 # >
